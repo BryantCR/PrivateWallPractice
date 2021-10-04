2f22ef3755a3fa080ca47b6908b39f6fd99df9d3
BryantCR <bryancvargas20@gmail.com>
2021-10-04 11:36:13 -0600

    ok

---
+++ login_app/controllers/users_controller.py
@@ -0,0 +1,62 @@
+from flask import render_template, request, redirect
+from login_app import app
+from login_app.models.User import User
+
+@app.route( "/" )
+def displayLogin():
+    return redirect ('/users')
+
+@app.route( "/users" )
+def displayAllUsers():
+    currentUsersInTable = User.get_all_users()
+    print(currentUsersInTable)
+    return render_template( "read.html", users1 = currentUsersInTable)
+
+@app.route('/users/new')
+def goToCreate():
+    return render_template( "create.html")
+
+@app.route('/users/create', methods=['POST'])
+def create():
+    print(request.form)
+    User.addDataForm(request.form)
+    return redirect('/users')
+
+#//////////////////////////////////////// EDIT PART ///////////////////////////////////////////
+#Here we're trying to redirect to the next page
+@app.route('/users/edit/<id>')
+def showUsersData(id):
+    updatedUsersInTable = User.get_one(id)
+    print("EditUserData: ", updatedUsersInTable)
+    return render_template("edit.html", users1 = updatedUsersInTable, id = id)
+
+#We're trying to got a form here
+@app.route('/users/show/<id>', methods=['POST'])
+def gotAndEditForm(id):
+    data = {
+        "first_name2Fromform2" : request.form['first_name2'],
+        "lastst_name2Fromform2" : request.form['last_name2'],
+        "email2Fromform2" : request.form['email2'],
+        "id" : id
+    }
+    edited = User.editUserData(data)
+    result = User.get_one(id)
+    print("EditUserData: ", result)
+    return render_template ("show.html", users1 = result, id = id, edited = edited )
+
+@app.route('/users/data/<id>')
+def showUsersDataInShowPage(id):
+    user = User.get_one(id)
+    print("1 :", user)
+    updatedUsersInTable = User.get_one(id)
+    print("EditUserData: ", updatedUsersInTable)
+    return render_template("show.html", users1 = updatedUsersInTable, id = id)
+
+@app.route("/users/delete/<id>")
+def deleteThisUser(id):
+    data={
+        'id': id
+    }
+    result2 = User.deleteUser(data)
+    print("EditUserData: ", result2)
+    return redirect('/users')
---
+++ login_app/models/User.py
@@ -0,0 +1,53 @@
+from users_app.confing.MySQLConnection import connectToMySQL
+from users_app import app 
+from datetime import date, datetime
+
+class User:
+    def __init__(self, id, first_name, last_name, email, created_at):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.created_at = created_at
+
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL("users_shema").query_db( query )
+        users = []
+        for n in results:
+            users.append( User( n['id'], n['first_name'], n['last_name'], n['email'], n['created_at'] ) )
+        return users
+    
+    @classmethod
+    def addDataForm(cls, data):
+        query = "INSERT INTO users (first_name , last_name , email, created_at, updated_at) VALUES ( %(first_name)s , %(last_name)s , %(email)s, SYSDATE(), SYSDATE());"
+        result = connectToMySQL('users_shema').query_db(query,data)
+        return result
+
+    @classmethod
+    def editUserData(cls, data):
+        query = "UPDATE users SET first_name = %(first_name2Fromform2)s, last_name = %(lastst_name2Fromform2)s, email = %(email2Fromform2)s, updated_at = SYSDATE() WHERE id=%(id)s;"
+        result = connectToMySQL('users_shema').query_db(query, data)
+        print(data)
+        return result
+
+    @classmethod
+    def get_one(cls, id):
+        print("8")
+        query  = "SELECT * FROM users WHERE id = %(id)s;"
+        data = {
+            "id" : id
+        }
+        result = connectToMySQL('users_shema').query_db( query, data )
+        user_data = []
+
+        #for users in result:
+            #user_data.append(User(user['id'],user['first_name'], user['last_name'], user['email'],user['created_at'],user['updated_at']))
+        print("9", user_data )
+        return result
+
+    @classmethod
+    def deleteUser(cls, data ):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_shema').query_db( query, data )
